9ba0b3c432b3c17f35c825000dd1584f0ef7d6e8
kuwadikadun <kuwadikadun@gmail.com>
2023-09-18 10:14:44 +0700

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::paginate(10);
+        return response()->json([
+            'data' => $customers
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customers = Customer::create([
+            'name' => $request->name,
+            'id_number' => $request->id_number,
+            'dob' => $request->dob,
+            'email' => $request->email,
+        ]);
+
+        return response()->json([
+            'data' => $customers
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return response()->json([
+            'data' => $customer
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        $customer->name = $request->name;
+        $customer->id_number = $request->id_number;
+        $customer->dob = $request->dob;
+        $customer->email = $request->email;
+        $customer->save();
+
+        return response()->json([
+            'data' => $customer
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return response()->json([
+            'message' => 'Customer Deleted!'
+        ]);
+    }
+}
